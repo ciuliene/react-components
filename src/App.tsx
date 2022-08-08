21fbbc47361f9e4d7a32b71f518ab63aa28c283c
Giuliano Errico <giuliano.errico@emerson.com>
2022-08-08 21:18:17 +0200

    Minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App: React.FC = () => {
         items={["ON", "OFF"]}
         colorOn="#F0D000"
       />
-      <BubbleLoader left="50vw" top="20vh" color="#20E080" ballSize={1} />
+      <BubbleLoader left="50vw" top="20vh" color="#20E080" bubbleSize={1} />
       <BounceLoader left="50vw" top="38vh" color="#2080E0" size={1.5} />
       <RadarLoader left="50vw" top="62vh" size={1} fill={false} />
     </div>
