41baf4ed476f8ee517f48e575c5fe6a23198e1a9
Giuliano Errico <giuliano.errico@emerson.com>
2022-08-11 07:29:55 +0200

    Minor fixes on Selector: - selector raises exception if id is not unique - unique id for row as well

--- src/components/Selector/index.tsx
+++ src/components/Selector/index.tsx
@@ -2,37 +2,13 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 
 type SelectorTableProps = {
+  id: string;
   active: boolean;
   colorOff?: string;
   colorOn?: string;
   textColor?: string;
   disabled?: boolean;
 };
-
-const SelectorTable = styled.table`
-  text-align: center;
-  transform: scale(1);
-  width: var(--width);
-  border-collapse: collapse;
-  table-layout: fixed;
-  background: ${(props: SelectorTableProps): string =>
-    !props.active ? props.colorOff || "#444" : props.colorOn || "#444"};
-  color: ${(props: SelectorTableProps): string => props.textColor || "#FFF"};
-  opacity: ${(props: SelectorTableProps): string =>
-    props.disabled ? "0.5" : "1"};
-  font-family: inherit;
-  border-radius: 100px;
-  cursor: pointer;
-  min-height: 30px;
-  min-width: 60px;
-  transition: all 0.2s;
-  -webkit-touch-callout: none;
-  -webkit-user-select: none;
-  -khtml-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-`;
 
 type SelectorPosProps = {
   active: boolean;
@@ -64,9 +40,49 @@ type SelectorProps = {
   className?: string;
 };
 
+const SelectorTable = styled.table`
+  text-align: center;
+  transform: scale(1);
+  width: var(--width);
+  border-collapse: collapse;
+  table-layout: fixed;
+  background: ${(props: SelectorTableProps): string =>
+    !props.active ? props.colorOff || "#444" : props.colorOn || "#444"};
+  color: ${(props: SelectorTableProps): string => props.textColor || "#FFF"};
+  opacity: ${(props: SelectorTableProps): string =>
+    props.disabled ? "0.5" : "1"};
+  font-family: inherit;
+  border-radius: 100px;
+  cursor: pointer;
+  min-height: 30px;
+  min-width: 60px;
+  transition: all 0.2s;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`;
+
+const SelectorRow = styled.tr`
+  & > * {
+    padding: 1px !important;
+  }
+`;
+
 export const Selector: React.FC<SelectorProps> = (props) => {
   useEffect(() => {
-    const r = document.querySelector<HTMLElement>("#" + props.id);
+    if (
+      document.querySelectorAll<HTMLElement>(`#selector-table-${props.id}`)
+        .length > 1
+    ) {
+      throw Error("The selector id must be unique");
+    }
+
+    const r = document.querySelector<HTMLElement>(
+      `#selector-table-${props.id}`
+    );
 
     if (r) {
       let length = 1.4;
@@ -79,7 +95,7 @@ export const Selector: React.FC<SelectorProps> = (props) => {
 
   return (
     <SelectorTable
-      id={props.id}
+      id={`selector-table-${props.id}`}
       className={props.className}
       onClick={(): any => {
         !props.disabled && props.onClick();
@@ -90,20 +106,18 @@ export const Selector: React.FC<SelectorProps> = (props) => {
       textColor={props.textColor}
       disabled={props.disabled}
     >
-      <thead>
-        <tr>
+      <tbody>
+        <SelectorRow>
           <th>
             <SelectorPos active={props.status} />
           </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
+        </SelectorRow>
+        <SelectorRow>
           <td>
             <span>{props.items ? props.items[0] : " "}</span>
           </td>
           <td>{props.items ? props.items[1] : " "}</td>
-        </tr>
+        </SelectorRow>
       </tbody>
     </SelectorTable>
   );
